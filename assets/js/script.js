d0fcca1a9ea9fa92458b448c4c9d1c36be4e1a1b
alperkan1 <seanhack@gmail.com>
2021-08-12 13:01:55 +0000

    next btn working now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-const strBtn =  document.getElementById("start")
+const strBtn = document.getElementById("start")
 const quizBox = document.getElementById("question-box")
+
 strBtn.addEventListener("click", beginGame)
 
-
+//* the start button starting the game**//
 function beginGame() {
-
+strBtn.classList.add("none")
+quizBox.classList.remove("none")
 }
 
-function forwardQuiz(){
+function forwardQuiz() {
 
 }
 
@@ -15,4 +17,3 @@ function selectAns() {
 
 
 }
-
