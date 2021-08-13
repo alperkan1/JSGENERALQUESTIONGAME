f85ed94d2e3907690b60d4d1fc3bee6d5884ea9e
alperkan1 <seanhack@gmail.com>
2021-08-13 13:59:03 +0000

    question shows

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,10 @@ strBtn.addEventListener("click", beginGame)
 //* the start button starting the game**//
 function beginGame() {
 strBtn.classList.add("none")
-quizBox.classList.remove("none")
-forwardQuiz()
 randomquiz = question.sort(() => Math.random() - .5)
 quest = 0
+quizBox.classList.remove("none")
+addNextQuest()
 }
 
 function addNextQuest() {
@@ -22,10 +22,13 @@ function addNextQuest() {
 }
 
 function VisualQ(question) {
+    questE.innerText=question.question
+
 
 }
 
 function forwardQuiz() {
+
 
 }
 
@@ -34,14 +37,14 @@ function selectAns() {
 
 }
 
-const question = {
-    q1:"Whats the Capital of Turkey?",
-    ans:[
+const question = [{
+    question:"Whats the Capital of Turkey?",
+    answer:[
         {text:"Ankara", correct: true},
         {text:"Istanbul", correct: false},
         {text:"Adana", correct: false},
         {tetx:"Antalya", correct:false}
     ]
 }
-
+]
  
